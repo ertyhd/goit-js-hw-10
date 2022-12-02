344af52984d6fe7cee2a202b92ee14fcb5fb7f86
ertyhd <orionship@protonmail.com>
2022-12-02 21:13:33 +0200

    Add loading challenge

--- src/index.js
+++ src/index.js
@@ -9,10 +9,12 @@ const refs = {
   countryList: document.querySelector('.country-list'),
   countryInfo: document.querySelector('.country-info'),
   tempForm: document.querySelector('.temp-form'),
+  loading: document.querySelector('body'),
 };
 
 const DEBOUNCE_DELAY = 300;
 let dataArr = [];
+// let hidenLoading = 'none';
 
 const renderList = () => {
   for (const i of dataArr) {
@@ -25,6 +27,7 @@ const renderList = () => {
 
 const renderInfo = () => {
   const languagesList = Object.values(dataArr[0].languages);
+  const capitalList = Object.values(dataArr[0].capital);
   refs.countryInfo.insertAdjacentHTML(
     'beforeend',
     `<div style="display: flex; align-items: center; "><img src="${
@@ -33,22 +36,29 @@ const renderInfo = () => {
       dataArr[0].name.official
     }</h2></div>
     <div >
-    <div style="display: flex; align-items: center; margin: 0; padding-left: 10px"><h3 style="margin: 0;">Capital: </h3><p style="margin: 0; padding-left: 10px">${
-      dataArr[0].capital[0]
-    }</p></div><div style="display: flex; align-items: center; margin: 0; padding-left: 10px"><h3 style="margin: 0;">Population: </h3><p style="margin: 0; padding-left: 10px">${
+    <div style="display: flex; align-items: center; margin: 0; padding-left: 10px"><h3 style="margin: 0;">Capital: </h3><p style="margin: 0; padding-left: 10px">${capitalList.join(
+      ', '
+    )}</p></div><div style="display: flex; align-items: center; margin: 0; padding-left: 10px"><h3 style="margin: 0;">Population: </h3><p style="margin: 0; padding-left: 10px">${
       dataArr[0].population
-    }</p></div><div style="display: flex; align-items: center; margin: 0; padding-left: 10px"><h3 style="margin: 0;">Languages: </h3><p  style="margin: 0;" padding-left: 10px">${languagesList.join(
+    }</p></div><div style="display: flex; align-items: center; margin: 0; padding-left: 10px"><h3 style="margin: 0; padding-right: 10px;">Languages: </h3><p style="margin: 0;" padding-left: 10px">${languagesList.join(
       ', '
     )}</p></div>
     </div>`
   );
 };
 
+refs.loading.insertAdjacentHTML(
+  'beforeend',
+  `<p class="load" Style="padding: 30px" >Loading...</p>`
+);
+let loadingChange = document.querySelector('.load');
+loadingChange.style.display = 'none';
+
 const handleSearch = event => {
   event.preventDefault();
-
   const inputValue = event.target.value;
   if (inputValue.length != 0) {
+    loadingChange.style.display = 'block';
     fetchCountries(inputValue)
       .then(data => {
         dataArr = data;
@@ -64,15 +74,20 @@ const handleSearch = event => {
       })
       .catch(() => {
         Notiflix.Notify.failure('Oops, there is no country with that name');
+      })
+      .finally(() => {
+        loadingChange.style.display = 'none';
       });
   }
   refs.countryInfo.innerHTML = '';
   refs.countryList.innerHTML = '';
+  return;
 };
 
 refs.searchBox.addEventListener(
   'input',
   debounce(handleSearch, DEBOUNCE_DELAY)
 );
+
 refs.countryList.style.listStyle = 'none';
 refs.countryList.style.padding = '0';
